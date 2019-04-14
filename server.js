2c2b648797fd2d2f995f37229a3815655c44edd5
espencer127 <evanispencer@gmail.com>
2019-04-14 14:57:30 -0400

    modifying to add gcal code

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
+const router = express.Router();
+const CAL_ID = "nmoogdmfcnoe16m5l5enuauhvc@group.calendar.google.com";
+var events;
+
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
@@ -47,13 +51,117 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/api/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
-    } else {
-      res.status(200).json(docs);
-    }
+
+  const fs = require('fs');
+  const readline = require('readline');
+  const  {google}  = require('googleapis');
+
+  // If modifying these scopes, delete token.json.
+  const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+  const TOKEN_PATH = 'token.json';
+
+  // Load client secrets from a local file.
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Calendar API.
+    authorize(JSON.parse(content), listEvents);
   });
+
+  /**
+  * Create an OAuth2 client with the given credentials, and then execute the
+  * given callback function.
+  * @param {Object} credentials The authorization client credentials.
+  * @param {function} callback The callback to call with the authorized client.
+  */
+  function authorize(credentials, callback) {
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return getAccessToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
+  }
+
+  /**
+  * Get and store new token after prompting for user authorization, and then
+  * execute the given callback with the authorized OAuth2 client.
+  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+  * @param {getEventsCallback} callback The callback for the authorized client.
+  */
+  function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+      });
+    });
+  }
+
+  /**
+  * Lists the next 10 events on the user's primary calendar.
+  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+  */
+  function listEvents(auth) {
+
+    try {
+      const calendar = google.calendar({ version: 'v3', auth });
+      calendar.events.list(
+        {
+          calendarId: CAL_ID,
+          timeMin: (new Date()).toISOString(),
+          maxResults: 10,
+          singleEvents: true,
+          orderBy: 'startTime',
+        },
+        (err, response) => {
+          if (err) return console.log('The API returned an error: ' + err);
+          this.events = response.data.items;
+          res.send(this.events);
+          if (this.events.length) {
+            console.log('Upcoming 10 events:');
+            this.events.map((event, i) => {
+              const start = event.start.dateTime || event.start.date;
+              console.log(`${start} - ${event.summary}`);
+            });
+          } else {
+            console.log('No upcoming events found.');
+          }
+        }
+      );
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  // db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+  //   if (err) {
+  //     handleError(res, err.message, "Failed to get contacts.");
+  //   } else {
+  //     res.status(200).json(docs);
+  //   }
+  // });
+
 });
 
 app.post("/api/contacts", function(req, res) {
